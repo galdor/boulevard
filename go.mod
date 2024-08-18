f59460744f1741a1b62e1d88b026a1cbd8cdf367
Nicolas Martyanoff <nicolas@n16f.net>
2024-08-18 13:44:06 +0200

    use go-yamlutils

--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	go.n16f.net/ejson v0.0.0-20240707135936-27789a89e2f3
 	go.n16f.net/log v0.0.0-20240707140441-2f7ab8f954a8
 	go.n16f.net/program v0.0.0-20240714172550-28d5a141755e
-	gopkg.in/yaml.v3 v3.0.1
+	go.n16f.net/yamlutils v0.0.0-20240818114527-49c848554a17
 )
 
 require (
 	go.n16f.net/uuid v0.0.0-20240707135755-e4fd26b968ad // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/term v0.19.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/service/cfg.go
+++ pkg/service/cfg.go
@@ -1,15 +1,12 @@
 package service
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 
 	"go.n16f.net/ejson"
 	"go.n16f.net/log"
-	"gopkg.in/yaml.v3"
+	"go.n16f.net/yamlutils"
 )
 
 type ServiceCfg struct {
@@ -28,92 +25,5 @@ func (cfg *ServiceCfg) Load(filePath string) error {
 		return fmt.Errorf("cannot read %q: %w", filePath, err)
 	}
 
-	// XXX All this mess will be replace by a single call to yaml.Load once
-	// go.n16f.net/yaml is ready.
-
-	yamlDecoder := yaml.NewDecoder(bytes.NewReader(data))
-
-	var yamlValue any
-	if err := yamlDecoder.Decode(&yamlValue); err != nil && err != io.EOF {
-		return fmt.Errorf("cannot decode YAML data: %w", err)
-	}
-
-	jsonValue, err := YAMLValueToJSONValue(yamlValue)
-	if err != nil {
-		return fmt.Errorf("invalid YAML data: %w", err)
-	}
-
-	jsonData, err := json.Marshal(jsonValue)
-	if err != nil {
-		return fmt.Errorf("cannot generate JSON data: %w", err)
-	}
-
-	if err := ejson.Unmarshal(jsonData, cfg); err != nil {
-		return fmt.Errorf("cannot decode JSON data: %w", err)
-	}
-
-	return nil
-}
-
-func YAMLValueToJSONValue(yamlValue any) (any, error) {
-	// For some reason, go-yaml will return objects as map[string]any
-	// if all keys are strings, and as map[any]any if not. So
-	// we have to handle both.
-
-	var jsonValue any
-
-	switch v := yamlValue.(type) {
-	case []any:
-		array := make([]any, len(v))
-
-		for i, yamlElement := range v {
-			jsonElement, err := YAMLValueToJSONValue(yamlElement)
-			if err != nil {
-				return nil, err
-			}
-
-			array[i] = jsonElement
-		}
-
-		jsonValue = array
-
-	case map[any]any:
-		object := make(map[string]any)
-
-		for key, yamlEntry := range v {
-			keyString, ok := key.(string)
-			if !ok {
-				return nil,
-					fmt.Errorf("object key \"%v\" is not a string", key)
-			}
-
-			jsonEntry, err := YAMLValueToJSONValue(yamlEntry)
-			if err != nil {
-				return nil, err
-			}
-
-			object[keyString] = jsonEntry
-		}
-
-		jsonValue = object
-
-	case map[string]any:
-		object := make(map[string]any)
-
-		for key, yamlEntry := range v {
-			jsonEntry, err := YAMLValueToJSONValue(yamlEntry)
-			if err != nil {
-				return nil, err
-			}
-
-			object[key] = jsonEntry
-		}
-
-		jsonValue = object
-
-	default:
-		jsonValue = yamlValue
-	}
-
-	return jsonValue, nil
+	return yamlutils.Load(data, cfg)
 }
