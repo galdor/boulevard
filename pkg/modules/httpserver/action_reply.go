0bfa9d4b1a4aa12ea4b75526e1e3164b22f648b2
Nicolas Martyanoff <nicolas@n16f.net>
2024-09-15 16:42:23 +0200

    use 200 as default status code for the reply action

--- pkg/modules/httpserver/action_reply.go
+++ pkg/modules/httpserver/action_reply.go
@@ -7,14 +7,16 @@ import (
 )
 
 type ReplyActionCfg struct {
-	Status int    `json:"status"`
+	Status int    `json:"status,omitempty"`
 	Reason string `json:"reason,omitempty"`
 	Header Header `json:"header,omitempty"`
 	Body   string `json:"body,omitempty"`
 }
 
 func (cfg *ReplyActionCfg) ValidateJSON(v *ejson.Validator) {
-	v.CheckIntMinMax("status", cfg.Status, 200, 599)
+	if cfg.Status != 0 {
+		v.CheckIntMinMax("status", cfg.Status, 200, 599)
+	}
 }
 
 type ReplyAction struct {
@@ -39,6 +41,11 @@ func (a *ReplyAction) Stop() {
 }
 
 func (a *ReplyAction) HandleRequest(ctx *RequestContext) {
+	status := 200
+	if a.Cfg.Status != 0 {
+		status = a.Cfg.Status
+	}
+
 	a.Cfg.Header.Apply(ctx.ResponseWriter.Header())
-	ctx.Reply(a.Cfg.Status, strings.NewReader(a.Cfg.Body))
+	ctx.Reply(status, strings.NewReader(a.Cfg.Body))
 }
