8efe6ab6bbc4ab608eb4992161144a84b007a0ad
Nicolas Martyanoff <nicolas@n16f.net>
2025-02-22 13:46:36 +0100

    fill HTTP server status data

--- pkg/protocols/http/status.go
+++ pkg/protocols/http/status.go
@@ -1,8 +1,13 @@
 package http
 
 type Status struct {
+	NbConnections int64 `json:"nb_connections"`
 }
 
 func (p *Protocol) StatusData() any {
-	return &Status{}
+	var status Status
+
+	status.NbConnections = p.nbConnections.Load()
+
+	return &status
 }
